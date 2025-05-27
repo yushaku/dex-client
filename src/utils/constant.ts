30434c464940fab2bf2770f3eb4122886357def6
yushaku <levanson180200@gmail.com>
2025-05-27 22:31:19 +0700

    remove no need keys

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -4,18 +4,14 @@ export const env = cleanEnv(import.meta.env, {
   VITE_WALLET_CONNECT_ID: str(),
   VITE_INFURA_KEY: str(),
   VITE_MORALIS_API_KEY: str(),
-  VITE_THIRD_WEB: str(),
-  VITE_THIRD_WEB_SECRET: str(),
+  VITE_PRIVY_APP_ID: str(),
   VITE_OWNER_ADDRESS: str(),
-  VITE_GRAPHQL_CMS_ENDPOINT: str(),
   VITE_SUBGRAPH_TOKEN: str(),
-  VITE_GRAPHQL_CMS_TOKEN: str(),
   VITE_PINATA_KEY: str(),
   VITE_PINATA_SECRET: str(),
   VITE_API_ENPOINT: str({ default: 'http://localhost:8080' }),
   VITE_ENV: str({ default: 'development' }),
-  VITE_ENABLE_TESTNETS: str({ default: 'false' }),
-  VITE_TENDERLY_VIRTUAL_RPC: str(),
+  VITE_ALCHEMY_API_KEY: str(),
 })
 
 export const routes = {
@@ -38,22 +34,11 @@ export const routes = {
 export const UNKNOWN_TOKEN = 'https://cdn.thena.fi/assets/UNKNOWN_TOKEN.png'
 export const GATEWAY_URL = 'https://ipfs.io/ipfs/'
 export const TOKEN_LIST = 'https://gateway.ipfs.io/ipns/tokens.uniswap.org'
-export const UNI = '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984'
-export const JSON_RPC_URL = 'https://cloudflare-eth.com'
-export const JSON_RPC = {
-  1: [`https://mainnet.infura.io/v3/${env.VITE_INFURA_KEY}`],
-  5: [`https://goerli.infura.io/v3/${env.VITE_INFURA_KEY}`],
-}
 
 export const YSK_ADDRESS = '0x7AFa15757A8012C3ECc0948154AD0f99c3b3c116'
-export const USDT_ADDRESS = '0x7AFa15757A8012C3ECc0948154AD0f99c3b3c116'
-export const NFT_ADRESS = '0x14a9c99d89106F66C2B86910d2C622Ce0A58C630'
 export const STAKE_ADRESS = '0xb407fFcC4D82295790D684F812d97EFdbB6c3122'
-export const MARKETPLACE_ADDRESS = '0x7175AAA9f0b6B05fe713AEFDD7B0026e61bd7aC5'
 export const NFT_FACTORY_ADDRESS = '0x64FFE32eCb2D433fc0868920c98Bf33Bee4f072A'
 export const PUBLIC_NFTS_ADDRESS = '0x284C5d066a4A2fD0163D190887fC5EFB6b4E0540'
-export const SHOP_PAYMENT_ADDRESS = '0x6b3De2f71bbeeEc360Cd50f6C19Daf13534bE8EA'
-
 export const LOGIN_MESSAGE =
   'Welcome to Yushaku! Please sign this message to verify account ownership.'
 
--- src/utils/wagmiConfig.ts
+++ src/utils/wagmiConfig.ts
@@ -18,27 +18,11 @@ import {
   waitForTransactionReceipt as _waitForTransactionReceipt,
   writeContract as _writeContract,
 } from '@wagmi/core'
-import { Abi, Address, defineChain } from 'viem'
+import { Abi, Address } from 'viem'
 import { createConfig, fallback, http, unstable_connector } from 'wagmi'
 import { arbitrum, bsc, bscTestnet, mainnet } from 'wagmi/chains'
 import { injected } from 'wagmi/connectors'
 import { env } from './constant'
-
-const vMainnet = defineChain({
-  ...mainnet,
-  id: 73571,
-  name: 'Virtual Ethereum Mainnet',
-  nativeCurrency: { name: 'vEther', symbol: 'vETH', decimals: 18 },
-  rpcUrls: {
-    default: { http: [env.VITE_TENDERLY_VIRTUAL_RPC!] },
-  },
-  blockExplorers: {
-    default: {
-      name: 'Tenderly Explorer',
-      url: 'https://dashboard.tenderly.co/explorer/vnet/47cdac98-cda3-431a-8fce-9f31037a3d0c',
-    },
-  },
-})
 
 const connectors = connectorsForWallets(
   [
@@ -82,13 +66,7 @@ export const walletTheme = {
   },
 }
 
-export const supportedChain = [
-  mainnet,
-  bsc,
-  arbitrum,
-  bscTestnet,
-  ...(env.VITE_ENABLE_TESTNETS === 'true' ? [bscTestnet, vMainnet] : []),
-] as const
+export const supportedChain = [mainnet, bsc, arbitrum, bscTestnet] as const
 
 export const config = createConfig({
   ssr: true,
@@ -102,7 +80,6 @@ export const config = createConfig({
     [arbitrum.id]: fallback([unstable_connector(injected), http()]),
     [bsc.id]: fallback([unstable_connector(injected), http()]),
     [bscTestnet.id]: fallback([unstable_connector(injected), http()]),
-    [vMainnet.id]: fallback([unstable_connector(injected), http()]),
   },
 }) as any
 
